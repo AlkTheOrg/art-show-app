9204dd52392e6eca759d0cb8300861d25e4b9532
Alkan <alkanorgun@gmail.com>
2021-02-13 14:57:07 +0300

    Initial commit

---
+++ src/main/java/org/alkan/artshowapp/ArtShowAppApplication.java
@@ -0,0 +1,13 @@
+package org.alkan.artshowapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtShowAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtShowAppApplication.class, args);
+    }
+
+}
