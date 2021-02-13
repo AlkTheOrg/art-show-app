d92e5132c56f52e8124ce81575cef69922603c5f
Alkan <alkanorgun@gmail.com>
2021-02-13 21:26:27 +0300

    Update on BaseEntity #2

--- src/main/java/org/alkan/artshowapp/models/BaseEntity.java
+++ src/main/java/org/alkan/artshowapp/models/BaseEntity.java
@@ -1,2 +1,26 @@
-package org.alkan.artshowapp.models;public class BaseEntity {
+package org.alkan.artshowapp.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public BaseEntity(Long id) {
+        this.id = id;
+    }
 }
