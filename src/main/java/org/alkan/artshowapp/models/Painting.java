ca075053667f000626c5fe235781d87b9eb48e76
Alkan <alkanorgun@gmail.com>
2021-02-13 21:23:17 +0300

    Created and implemented Painting

---
+++ src/main/java/org/alkan/artshowapp/models/Painting.java
@@ -0,0 +1,52 @@
+package org.alkan.artshowapp.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "paintings")
+public class Painting extends Artwork{
+
+    private float length;
+    private float width;
+
+    public Painting(Long id, String name, String style, float length, float width) {
+        super(id, name, style);
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public int calculateCost() {
+        String style = getStyle();
+        double uniquePrice = uniquePrice(style);
+        int cost = (int) (length * width * uniquePrice);
+
+        return cost;
+    }
+
+    private double uniquePrice(String style) {
+        double result = 0.0;
+
+        switch (style) {
+            case ("Renaissance"):
+                result = 7.0;
+                break;
+            case ("Baroque"):
+                result = 5.5;
+                break;
+            default:
+                result = 4.5;
+                break;
+        }
+        return result;
+    }
+}
