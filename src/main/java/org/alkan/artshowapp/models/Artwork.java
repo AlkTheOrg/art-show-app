1cc219092042684dd1f3d1c41b3d86f91d9c8f0f
Alkan <alkanorgun@gmail.com>
2021-02-13 21:22:19 +0300

    Created Base Artwork Entity. Closes #3

---
+++ src/main/java/org/alkan/artshowapp/models/Artwork.java
@@ -0,0 +1,32 @@
+package org.alkan.artshowapp.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@MappedSuperclass
+public abstract class Artwork extends BaseEntity {
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "style")
+    private String style;
+
+    //  private ARTIST TODO;
+
+    public Artwork(Long id, String name, String style) {
+        super(id);
+        this.name = name;
+        this.style = style;
+    }
+
+    public abstract int calculateCost();
+}
