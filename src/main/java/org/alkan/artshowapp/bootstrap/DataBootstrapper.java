0b2c427f8fcd0aee28e8270031a52c0bcc4af0a9
Alkan <alkanorgun@gmail.com>
2021-02-15 23:27:44 +0300

    Created Style Repo and updated bootstrapper. Closes #22

--- src/main/java/org/alkan/artshowapp/bootstrap/DataBootstrapper.java
+++ src/main/java/org/alkan/artshowapp/bootstrap/DataBootstrapper.java
@@ -1,7 +1,9 @@
 package org.alkan.artshowapp.bootstrap;
 
+import org.alkan.artshowapp.models.Style;
 import org.alkan.artshowapp.models.artworks.Painting;
 import org.alkan.artshowapp.repositories.PaintingRepository;
+import org.alkan.artshowapp.repositories.StyleRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -9,17 +11,29 @@ import org.springframework.stereotype.Component;
 public class DataBootstrapper implements CommandLineRunner {
 
     private final PaintingRepository paintings;
+    private final StyleRepository styles;
 
-    public DataBootstrapper(PaintingRepository paintings) {
+    public DataBootstrapper(PaintingRepository paintings, StyleRepository styles) {
         this.paintings = paintings;
+        this.styles = styles;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        paintings.save(new Painting(1L, "name1", "horror1", 12.72f, 24.3f));
-        paintings.save(new Painting(2L, "name2", "horror2", 13.72f, 24.3f));
-        paintings.save(new Painting(3L, "name3", "horror3", 14.72f, 24.3f));
-        paintings.save(new Painting(4L, "name4", "horror4", 15.72f, 24.3f));
-        paintings.save(new Painting(5L, "name5", "horror5", 16.72f, 24.3f));
+        Style baroque = new Style();
+        baroque.setName("baroq");
+        baroque.setId(1L);
+        styles.save(baroque);
+
+        Style renosance = new Style();
+        renosance.setName("renosance");
+        renosance.setId(2L);
+        styles.save(renosance);
+
+        paintings.save(new Painting(1L, "name1", renosance, 12.72f, 24.3f));
+        paintings.save(new Painting(2L, "name2", baroque, 13.72f, 24.3f));
+        paintings.save(new Painting(3L, "name3", renosance, 14.72f, 24.3f));
+        paintings.save(new Painting(4L, "name4", baroque, 15.72f, 24.3f));
+        paintings.save(new Painting(5L, "name5", renosance, 16.72f, 24.3f));
     }
 }
---
+++ src/main/java/org/alkan/artshowapp/repositories/StyleRepository.java
@@ -0,0 +1,7 @@
+package org.alkan.artshowapp.repositories;
+
+import org.alkan.artshowapp.models.Style;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StyleRepository extends JpaRepository<Style, Long> {
+}
