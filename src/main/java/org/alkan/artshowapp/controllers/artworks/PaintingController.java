3ad8eaa416361d260f28f4ace07ae3e90fafb2f9
Alkan <alkanorgun@gmail.com>
2021-02-16 19:34:32 +0300

    Impl of view functionality of painting. Closes #46

--- src/main/java/org/alkan/artshowapp/controllers/artworks/PaintingController.java
+++ src/main/java/org/alkan/artshowapp/controllers/artworks/PaintingController.java
@@ -1,13 +1,17 @@
 package org.alkan.artshowapp.controllers.artworks;
 
+import lombok.extern.slf4j.Slf4j;
 import org.alkan.artshowapp.repositories.artworks.PaintingRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/artworks/paintings")
 @Controller
+@Slf4j
 public class PaintingController {
 
     private final PaintingRepository paintings;
@@ -21,6 +25,13 @@ public class PaintingController {
         model.addAttribute("paintings", paintings.findAll());
 
         return "artworks/paintings/index";
+    }
+
+    @GetMapping({"/{paintingId}", "/{paintingId}/"})
+    public ModelAndView showPainting(@PathVariable("paintingId") Long paintingId) {
+        ModelAndView mav = new ModelAndView("/artworks/paintings/show");
+        mav.addObject(paintings.findById(paintingId).orElse(null));
+        return mav;
     }
 
 }
