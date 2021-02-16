ce385d8d8496eaa8d778423f38b4cfc69564d624
Alkan <alkanorgun@gmail.com>
2021-02-16 16:19:09 +0300

    Created Architect model. Closes #9

---
+++ src/main/java/org/alkan/artshowapp/models/people/Architect.java
@@ -0,0 +1,19 @@
+package org.alkan.artshowapp.models.people;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table
+public class Architect extends Person{
+
+}
