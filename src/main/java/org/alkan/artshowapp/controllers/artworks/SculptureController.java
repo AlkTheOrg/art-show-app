5dc1208a2e0655b2af1777fb75d618504e11b005
Alkan <alkanorgun@gmail.com>
2021-02-20 16:59:35 +0300

    Implemented Sculpture view. Closes #57.'

--- src/main/java/org/alkan/artshowapp/controllers/artworks/SculptureController.java
+++ src/main/java/org/alkan/artshowapp/controllers/artworks/SculptureController.java
@@ -4,6 +4,7 @@ import org.alkan.artshowapp.repositories.artworks.SculptureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/artworks/sculptures")
@@ -20,5 +21,11 @@ public class SculptureController {
     public String listSculptures(Model model) {
         model.addAttribute("sculptures", sculptures.findAll());
         return "artworks/sculptures/index";
+    }
+
+    @GetMapping({"/{sculptureId}", "/{sculptureId}/"})
+    public String showPainting(@PathVariable("sculptureId") Long sculptureId, Model model) {
+        model.addAttribute("sculpture", sculptures.findById(sculptureId).orElse(null));
+        return "/artworks/sculptures/show";
     }
 }
