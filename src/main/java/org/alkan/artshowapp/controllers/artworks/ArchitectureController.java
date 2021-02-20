6ae9a02778f417a7d028ce3f420f2187a762e47e
Alkan <alkanorgun@gmail.com>
2021-02-20 15:19:59 +0300

    Implemented Architecture View. Closes #56

--- src/main/java/org/alkan/artshowapp/controllers/artworks/ArchitectureController.java
+++ src/main/java/org/alkan/artshowapp/controllers/artworks/ArchitectureController.java
@@ -4,7 +4,9 @@ import org.alkan.artshowapp.repositories.artworks.ArchitectureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/artworks/architectures")
 @Controller
@@ -20,5 +22,12 @@ public class ArchitectureController {
     public String listArchitectures(Model model) {
         model.addAttribute("architectures", architectures.findAll());
         return "artworks/architectures/index";
+    }
+
+    @GetMapping({"/{architectureId}", "/{architectureId}/"})
+    public ModelAndView showArchitecture(@PathVariable("architectureId") Long architectureId) {
+        ModelAndView mav = new ModelAndView("/artworks/architectures/show");
+        mav.addObject(architectures.findById(architectureId).orElse(null));
+        return mav;
     }
 }
