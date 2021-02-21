87d7d7d6147bebf30e61ff775ac254438ccf9b0d
Alkan <alkanorgun@gmail.com>
2021-02-22 00:41:37 +0300

    Implemented Show Architect request. Closes #64

--- src/main/java/org/alkan/artshowapp/controllers/people/ArchitectController.java
+++ src/main/java/org/alkan/artshowapp/controllers/people/ArchitectController.java
@@ -1,9 +1,11 @@
 package org.alkan.artshowapp.controllers.people;
 
+import org.alkan.artshowapp.models.people.Architect;
 import org.alkan.artshowapp.repositories.people.ArchitectRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/people/architects")
@@ -20,5 +22,13 @@ public class ArchitectController {
     public String listArchitects(Model model) {
         model.addAttribute("architects", architects.findAll());
         return "people/architects/index";
+    }
+
+    @GetMapping({"/{architectId}", "/{architectId}/"})
+    public String showArchitect(@PathVariable("architectId") Long architectId, Model model) {
+        Architect architect = architects.findById(architectId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid id: " + architectId));
+        model.addAttribute("architect", architect);
+        return "people/architects/show";
     }
 }
