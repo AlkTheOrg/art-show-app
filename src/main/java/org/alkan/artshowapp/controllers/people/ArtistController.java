83886e0acecb6a2769ef0677bec53d563a067697
Alkan <alkanorgun@gmail.com>
2021-02-21 21:33:44 +0300

    Implemented Create Artist Request. Closes #61

--- src/main/java/org/alkan/artshowapp/controllers/people/ArtistController.java
+++ src/main/java/org/alkan/artshowapp/controllers/people/ArtistController.java
@@ -1,21 +1,28 @@
 package org.alkan.artshowapp.controllers.people;
 
+import lombok.extern.slf4j.Slf4j;
 import org.alkan.artshowapp.models.people.Artist;
+import org.alkan.artshowapp.repositories.PeriodRepository;
 import org.alkan.artshowapp.repositories.people.ArtistRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RequestMapping("/people/artists")
 @Controller
+@Slf4j
 public class ArtistController {
 
     private final ArtistRepository artists;
+    private final PeriodRepository periods;
 
-    public ArtistController(ArtistRepository artists) {
+    public ArtistController(ArtistRepository artists, PeriodRepository periods) {
         this.artists = artists;
+        this.periods = periods;
     }
 
     @GetMapping({"", "/", "/index", "/index/"})
@@ -31,5 +38,29 @@ public class ArtistController {
                 .orElseThrow(() -> new IllegalArgumentException("Invalid id: " + id));
         model.addAttribute("artist", artist);
         return "/people/artists/show";
+    }
+
+    @GetMapping({"/new", "/new/"})
+    public String showCreationForm(Model model) {
+        model.addAttribute("periods", periods.findAll());
+        model.addAttribute("artist", new Artist());
+        return "people/artists/new";
+    }
+
+    @PostMapping({"", "/"})
+    public String processCreationForm(@Valid Artist artist, BindingResult result,
+                  @RequestParam(name = "artist-is-alive", defaultValue = "off") String isAlive) {
+        if(result.hasErrors()) {
+            for(ObjectError err : result.getAllErrors())
+                System.out.println(err.getDefaultMessage());
+            return "people/artists/new";
+        }
+        else {
+            if(isAlive.equals("on"))
+                artist.setDeathYear(null);
+            artist.getPeriods().forEach((period) -> period.getArtists().add(artist));
+            Artist savedArtist = artists.save(artist);
+            return "redirect:/people/artists/" + savedArtist.getId();
+        }
     }
 }
