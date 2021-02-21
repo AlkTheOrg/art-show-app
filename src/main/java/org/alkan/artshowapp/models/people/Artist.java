5fe555fdf6ef595d438df794daa65835ce0677e8
Alkan <alkanorgun@gmail.com>
2021-02-21 23:19:15 +0300

    Added CascadeType.All to OneToMany relationships of Artist. #69

--- src/main/java/org/alkan/artshowapp/models/people/Artist.java
+++ src/main/java/org/alkan/artshowapp/models/people/Artist.java
@@ -5,10 +5,7 @@ import org.alkan.artshowapp.models.Period;
 import org.alkan.artshowapp.models.artworks.Painting;
 import org.alkan.artshowapp.models.artworks.Sculpture;
 
-import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.Year;
 import java.util.HashSet;
 import java.util.Set;
@@ -24,10 +21,10 @@ public class Artist extends Person {
     @ManyToMany(mappedBy = "artists")
     private Set<Period> periods = new HashSet<>();
 
-    @OneToMany(mappedBy = "artist")
+    @OneToMany(mappedBy = "artist", cascade = CascadeType.ALL)
     private Set<Painting> paintings = new HashSet<>();
 
-    @OneToMany(mappedBy = "artist")
+    @OneToMany(mappedBy = "artist", cascade = CascadeType.ALL)
     private Set<Sculpture> sculptures = new HashSet<>();
 
     @Builder
